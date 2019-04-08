78682e0787cee426eeae52f04170475142cd8d98
Chris Xiao <csx773@gmail.com>
2019-04-08 18:54:07 +0000

    finished step2

--- httpExercise.js
+++ httpExercise.js
@@ -23,8 +23,35 @@ function getAndPrintHTMLChunks () {
 
 }
 
-getAndPrintHTMLChunks();
+function getAndPrintHTML () {
 
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+  var completeData = null;
+  https.get(requestOptions, function (response){
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      completeData += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log('Complete data is: \n', completeData);
+    });
+
+  })
+
+}
+ // Testing code below
+//getAndPrintHTMLChunks();
+getAndPrintHTML();
 
 /*
 // notice that https.get takes a callback with one parameter -
